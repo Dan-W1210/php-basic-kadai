3d7c4614c8606735d62fb77675284c9d31224f92
Dan-W1210 <giulietta33@o3i5o.com>
2025-12-07 12:27:05 +0900

    課題再提出

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -19,7 +19,9 @@
       $score9 = 30;
       $score10 = 60;
 
-      echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10 / 10;
+      $score_total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+      echo $score_total / 10;
       ?>
 
     </p>
